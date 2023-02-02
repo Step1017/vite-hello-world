a01b5badfe07c4a5d512de9c09b5df5ab2007d0f
Step1017 <s.aveta@yahoo.com>
2023-02-02 15:10:52 +0100

    Esercizio Completato

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppTitle',
+        data() {
+            return {
+                msg: 'La mia prima app con Vite!',
+                count: 0
+            }
+        }
+}
+</script>
+
+<template>
+    <h1>
+        {{ msg }}
+    </h1>
+</template>
+
+<style scoped>
+</style>
